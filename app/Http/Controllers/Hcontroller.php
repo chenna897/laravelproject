41ed3b64dcb73576860c56e27ba5b0795c172f02
Emile MILOT <emile.milot@gmail.com>
2019-10-15 16:38:39 +0530

    Encryption&Hashing

--- app/Http/Controllers/Hcontroller.php
+++ app/Http/Controllers/Hcontroller.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
 
 class Hcontroller extends Controller
 {
@@ -34,12 +36,20 @@ class Hcontroller extends Controller
     public function ajaxRequestPost(Request $request)
     {
         $input = $request->all();
-        //  echo json_encode($input);
-         echo $input['name'];
-         echo $input['password'];
-         echo $input['email'];
-        $data = User::create($input);
-        // return response()->json(['success'=>'Record inserted successfully'], 200);
+          echo json_encode($input);
+          echo $input['name'];
+          echo '                    ';
+          echo $input['email'];
+          echo '                     ';
+          echo $encrypted  = Crypt::encryptString($input['password']);
+          echo '                                                    ';
+          echo $decrypted = Crypt::decryptString($encrypted);
+          echo '                                                    ';
+          $input['password'] = $encrypted;
+        //   echo $hashed = bcrypt($input['password']);
+        //   $input['password'] = $hashed;
+          $data = User::create($input);
+          //return response()->json(['success'=>'Record inserted successfully'], 200);
 
     }
 
